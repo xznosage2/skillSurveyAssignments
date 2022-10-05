6d15cdc8b8a86dd39764324d3ee02315faa4d2fd
xznosage2 <gzellner@student.neumont.edu>
2022-10-05 09:13:55 -0600

    Update App.js

--- App.js
+++ App.js
@@ -3,18 +3,68 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+    <View style={styles.theHeader}>
+      <Text>Hedder</Text>
       <StatusBar style="auto" />
     </View>
+    
+    <View style={styles.personalInfo}>
+        <Text>personal informetion </Text>
+        <StatusBar style="auto" />
+      </View>
+      
+      <View style={styles.theSkills}>
+        <Text>Your skills</Text>
+        <StatusBar style="auto" />
+      </View>
+      
+      <View style={styles.theSubmission}>
+        <Text>Submit</Text>
+        <StatusBar style="auto" />
+      </View>
+      
+      </>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  theHeader: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'Black',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor:'red',
+    borderWidth:5,
   },
+  personalInfo: {
+    flex: 1,
+    backgroundColor: 'Red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:'black',
+    borderWidth:5,
+  },
+
+  theSkills: {
+    flex: 1,
+    backgroundColor: 'Orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:'black',
+    borderWidth:5,
+  },
+
+  theSubmission: {
+    flex: 1,
+    backgroundColor: 'Yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:'black',
+    borderWidth:5,
+  },
+
+
+
 });
