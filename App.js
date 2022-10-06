9467ede85adcc7da67d5ea3a9a21bc8b14a81e53
xznosage2 <gzellner@student.neumont.edu>
2022-10-06 08:07:37 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <>
+
     <View style={styles.theHeader}>
       <Text>Hedder</Text>
       <StatusBar style="auto" />
@@ -31,6 +32,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   theHeader: {
+    x: 0,
+    y:0,
+    width: 200,
+    height: 200,
     flex: 1,
     backgroundColor: 'Black',
     alignItems: 'center',
@@ -39,6 +44,10 @@ const styles = StyleSheet.create({
     borderWidth:5,
   },
   personalInfo: {
+    x: 0,
+    y:200,
+    width: 200,
+    height: 200,
     flex: 1,
     backgroundColor: 'Red',
     alignItems: 'center',
@@ -48,6 +57,10 @@ const styles = StyleSheet.create({
   },
 
   theSkills: {
+    x: 0,
+    y:400,
+    width: 200,
+    height: 200,
     flex: 1,
     backgroundColor: 'Orange',
     alignItems: 'center',
@@ -57,6 +70,10 @@ const styles = StyleSheet.create({
   },
 
   theSubmission: {
+    x: 0,
+    y:600,
+    width: 200,
+    height: 200,
     flex: 1,
     backgroundColor: 'Yellow',
     alignItems: 'center',
